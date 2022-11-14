81d431aba088d97caa3cd0f4cba34be5d76cbc84
Sophie <sandovalsofi99@gmail.com>
2022-11-14 20:18:49 -0300

    soy una genia

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import '../assets/css/app.css';
-
+import TopBar from './TopBar'
+import ContentRowTop from './ContentRowTop'
+import Sidebar from './Sidebar'
 function ContentWrapper(){
     return(
-        <div></div>
+        <div id="wrapper">
+        <Sidebar/>
+        <div id="content-wrapper" className="d-flex flex-column">
+            
+            <div id="content">
+			
+            <TopBar/>
+            <ContentRowTop/>
+			</div>
+
+        </div>
+        
+        </div>
         )
     }
 export default ContentWrapper;
