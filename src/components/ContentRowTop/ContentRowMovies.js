c74504aefa18016aefec4de2d49679e55ab2457a
Sophie <sandovalsofi99@gmail.com>
2022-12-21 11:00:38 -0300

    compertamentalizing

--- src/components/ContentRowTop/ContentRowMovies.js
+++ src/components/ContentRowTop/ContentRowMovies.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import '../../assets/css/app.css';
+import Card from './ContentRowMovies/Card';
 
-function ContentRowMovies(){
+function ContentRowMovies(props){
     return(
         <div className="row">
 
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-film fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						<Card
+                        titulo="Movies in Data Base" 
+                        cifra="21" 
+                        colorDeBorde="card border-left-primary shadow h-100 py-2"
+                        colorTitulo= "text-xs font-weight-bold text-primary text-uppercase mb-1"
+                        icono="fas fa-film fa-2x text-gray-300"
+                        />
+                        <Card
+                        titulo="Total awards" 
+                        cifra="79" 
+                        colorDeBorde="card border-left-success shadow h-100 py-2"
+                        colorTitulo="text-xs font-weight-bold text-success text-uppercase mb-1"
+                        icono="fas fa-award fa-2x text-gray-300"
+                        />
+                        <Card
+                        titulo="Total awards" 
+                        cifra="79" 
+                        colorDeBorde="card border-left-success shadow h-100 py-2"
+                        colorTitulo="text-xs font-weight-bold text-success text-uppercase mb-1"
+                        icono="fas fa-award fa-2x text-gray-300"
+                        />
+                        
+                        <Card/>
+                        <Card/>
 
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
 
 						<div className="col-md-4 mb-4">
 							<div className="card border-left-warning shadow h-100 py-2">
